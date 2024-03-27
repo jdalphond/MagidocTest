7abd1165c53a873fbae4e5c520fc7f93675808c8
James Dalphond <james.dalphond@gmail.com>
2024-03-27 12:51:15 -0400

    initial commit

---
+++ magidoc.mjs
@@ -0,0 +1,17 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    introspection: {
+      type: 'url',
+      url: 'http://localhost:4000',
+    },
+    website: {
+      template: 'carbon-multi-page',
+      staticAssets: path.join(__dirname, 'assets'),
+    },
+  }
